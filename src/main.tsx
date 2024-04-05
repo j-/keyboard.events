0ed282f55d713cb345270f4d6086149eb5cc9859
j- <j@skeoh.com>
2024-04-06 10:37:11 +1100

    Register service worker

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { registerSW } from 'virtual:pwa-register';
 import { App } from './App.tsx';
 import './index.css';
+
+if ('serviceWorker' in navigator) {
+  registerSW();
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
