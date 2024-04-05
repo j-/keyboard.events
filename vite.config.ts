844eef348663c5a4c615962d7f01d5493e4a9ac2
j- <j@skeoh.com>
2024-04-06 10:57:14 +1100

    Set "inject register" config to "auto"

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      injectRegister: 'auto',
       registerType: 'autoUpdate',
       workbox: {
         globPatterns: ['**/*'],
