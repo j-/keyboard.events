72fbae436f74128250ee3dbec92a50eff32ec91b
j- <j@skeoh.com>
2024-04-23 19:19:17 +1000

    Update "exit fullscreen" button copy

--- src/components/FullscreenButton.tsx
+++ src/components/FullscreenButton.tsx
@@ -1,14 +1,17 @@
 import classNames from 'classnames';
 import { FC, HTMLAttributes } from 'react';
-import { Button } from './Button';
 import { OS, useOS } from '../hooks/use-os';
+import { Button } from './Button';
 
 const FullscreenMessage: FC = () => {
   const os = useOS();
   switch (os) {
-    case OS.WIN: return <>Disables the Windows button, Ctrl+W, Alt+Tab, Alt+F4 etc.</>;
-    case OS.MAC: return <>Disables Cmd+W, Cmd+Q, Cmd+H etc.</>;
-    default: return null;
+    case OS.WIN:
+      return <>Disables the Windows button, Ctrl+W, Alt+Tab, Alt+F4 etc.</>;
+    case OS.MAC:
+      return <>Disables Cmd+W, Cmd+Q, Cmd+H etc.</>;
+    default:
+      return null;
   }
 };
 
