f62396b95b6ad8db296f389fa64f6bb3896be599
j- <j@skeoh.com>
2024-04-23 19:31:40 +1000

    Refactor "use OS" hook module

--- src/hooks/use-os.ts
+++ src/hooks/use-os.ts
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from 'react';
 
 export enum OS {
   /** MacOS */
@@ -20,35 +20,42 @@ const windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'];
 const iosPlatforms = ['iPhone', 'iPad', 'iPod'];
 
 // See https://stackoverflow.com/a/38241481
-export const getOS = (platform = window.navigator.platform, userAgent = window.navigator.userAgent): OS => {
-	if (macosPlatforms.includes(platform)) {
-		return OS.MAC;
-	} else if (iosPlatforms.includes(platform)) {
-		return OS.IOS;
-	} else if (windowsPlatforms.includes(platform)) {
-		return OS.WIN;
-	} else if (/Android/.test(userAgent)) {
-		return OS.AND;
-	} else if (/Linux/.test(platform)) {
-		return OS.LIN;
-	} else {
+export const getOS = (
+  platform = window.navigator.platform,
+  userAgent = window.navigator.userAgent,
+): OS => {
+  try {
+    if (macosPlatforms.includes(platform)) {
+      return OS.MAC;
+    } else if (iosPlatforms.includes(platform)) {
+      return OS.IOS;
+    } else if (windowsPlatforms.includes(platform)) {
+      return OS.WIN;
+    } else if (/Android/.test(userAgent)) {
+      return OS.AND;
+    } else if (/Linux/.test(platform)) {
+      return OS.LIN;
+    } else {
+      return OS.OTH;
+    }
+  } catch {
     return OS.OTH;
   }
 };
 
-const usePlatform = () => {
-  const [platform, setPlatform] = useState(() => window.navigator.platform);
-  useEffect(() => setPlatform(window.navigator.platform));
+const usePlatform = (navigator = window.navigator): string => {
+  const [platform, setPlatform] = useState(() => navigator.platform);
+  useEffect(() => setPlatform(navigator.platform), [navigator]);
   return platform;
 };
 
-const useUserAgent = () => {
-  const [userAgent, setUserAgent] = useState(() => window.navigator.userAgent);
-  useEffect(() => setUserAgent(window.navigator.userAgent));
+const useUserAgent = (navigator = window.navigator): string => {
+  const [userAgent, setUserAgent] = useState(() => navigator.userAgent);
+  useEffect(() => setUserAgent(navigator.userAgent), [navigator]);
   return userAgent;
 };
 
-export const useOS = () => {
+export const useOS = (): OS => {
   const platform = usePlatform();
   const userAgent = useUserAgent();
   const os = useMemo(() => getOS(platform, userAgent), [platform, userAgent]);
