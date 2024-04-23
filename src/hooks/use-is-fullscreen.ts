a20dced73a2529781fce27d88b25ae3009936eb4
j- <j@skeoh.com>
2024-04-23 19:22:21 +1000

    Init "is fullscreen" check on mount

--- src/hooks/use-is-fullscreen.ts
+++ src/hooks/use-is-fullscreen.ts
@@ -10,6 +10,7 @@ export const useIsFullscreen = () => {
   const [isFullscreen, setIsFullscreen] = useState(initIsFullscreen);
 
   useEffect(() => {
+    setIsFullscreen(initIsFullscreen);
     const handler = () => setIsFullscreen(initIsFullscreen);
     document.addEventListener('fullscreenchange', handler);
     return () => document.removeEventListener('fullscreenchange', handler);
